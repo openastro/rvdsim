cd4a6fdd77923688caeeeb34c64245ab54d5fe4d
Kartik Kumar <me@kartikkumar.com>
2016-04-19 22:41:18 +0200

    Fix copyright statements in file headers.

--- ProjectFiles.cmake
+++ ProjectFiles.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) <year> <author> (<email>)
+# Copyright (c) 2016 Kartik Kumar, Dinamica Srl (me@kartikkumar.com)
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
