6c69521c9a80c19857f57e16079d2815cfc41bd2
Kartik Kumar <me@kartikkumar.com>
2016-04-21 11:07:50 +0200

    Add pause in run_all bash script; wait for user to hit enter.

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -3,10 +3,19 @@
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
-set -ev
+function pause(){
+   read -p "$*"
+}
 
 # Run simulator and plotting scripts.
 # The path to the config files should be passed as a command-line parameter to this script.
+
+echo ''
+pause 'Press [Enter] to run the Rvdsim simulator ...'
 ../build/bin/rvdsim $1/rvdsim_config.json
+
+pause 'Press [Enter] to generate a plot of the chaser path ...'
 python ../python/plot_chaser_path.py $1/plot_chaser_path.json
+
+pause 'Press [Enter] to generate a plot of the chaser thrust profile ...'
 python ../python/plot_chaser_thrust.py $1/plot_chaser_thrust.json
